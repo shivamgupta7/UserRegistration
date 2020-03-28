bf929c075a0d423c19f318c6de9d547ca4f876b1
shivamg.cr7@gmail.com <shivamg.cr7@gmail.com>
2020-03-28 18:26:57 +0530

    [Shivam] Add valid email id for user registration.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -x
 
 shopt -s extglob
-echo "Enter Last Name (starts with Cap and has min 3 char): "
-function lastName()
+function emailID()
 {
-   read lname
-   lname_pat="^[A-Z][a-z]{2,}$"
-   if [[ $lname =~ $lname_pat ]]
+   echo "Enter Email Id :"
+   read email
+   email_pat="^([a-zA-Z]+[a-zA-Z0-9]*((\.|\-|\+|\_)?[A-Za-z0-9]*))@([a-zA-Z0-9]+)\.([a-z]{2,4})(\.[a-z]{2})?$"
+   if [[ $email =~ $email_pat ]]
    then
-      echo "Valid Last Name."
+      echo "Email is valid."
    else
-      echo "Invalid Last Name."
+      echo "Invalid Email."
    fi
 }
 
-lastName
+emailID
