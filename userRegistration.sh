60f98474d71344e9f7a84f6e00d727442bd246df
shivamg.cr7@gmail.com <shivamg.cr7@gmail.com>
2020-03-28 19:38:37 +0530

    [Shivam] Password should have at least 1 Upper Case.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
 shopt -s extglob
-function password_1()
+function password_2()
 {
-   echo "Enter password(minimun 8 char) :"
+   echo "Enter password(minimun 8 char & at least 1 Upper Case) :"
    read password
-   password_pat1="^[A-Za-z0-9]{8,}$"
-   if [[ $password =~ $password_pat1 ]]
+   password_pat2="^(?=.*[A-Z])[A-Za-z0-9]{8,}$"
+   if [[ $password =~ $password_pat2 ]]
    then
       echo "Password is valid."
    else
@@ -14,4 +14,4 @@ function password_1()
    fi
 }
 
-password_1
+password_2
